a1f267a6b3ba77d12383b3dec7cbc463332a8ccb
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-04 16:24:05 +0000

    Navlink component v1 (without tooltip for ng variant)

---
+++ src/components/atoms/NavLink.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+interface NavLinkProps {
+  href: string;
+  children: string;
+  variant?: 'bg' | 'sm';
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, children, variant = 'sm' }) => {
+
+  const linkClasses = `
+    font-messina-semi-bold
+    text-nav-${variant} 
+    hover:underline 
+    ${variant === 'bg' ? 'hover:decoration-6' : 'hover:decoration-3'}
+    underline-offset-4 
+    transition-all 
+    duration-300
+  `;
+
+  return (
+      <Link href={href} className={linkClasses}>
+        {children}
+      </Link>
+  );
+};
+
+export default NavLink;
