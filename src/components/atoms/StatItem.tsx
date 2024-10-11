1455eef3c7a1d54deeb051ae05d8aece5f850a8d
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-11 15:47:36 +0000

    fix style

--- src/components/atoms/StatItem.tsx
+++ src/components/atoms/StatItem.tsx
@@ -7,8 +7,8 @@ interface StatItemProps {
 
 const StatItem: React.FC<StatItemProps> = ({ title, value }) => {
   return (
-    <div className="flex w-[150px] p-[20px_20px] flex-col justify-center items-center gap-[20px] self-stretch border border-black/50">
-      <Typography variant="xs">
+    <div className="flex w-[18ch] p-[25px_10px] flex-col justify-center items-center gap-[20px] self-stretch border border-black/50">
+      <Typography variant="xxs">
         {title}
       </Typography>
       <Typography variant="h5">
