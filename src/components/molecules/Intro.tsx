78ea6a6a3c3dc82020f72c89710b9c5550440d24
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-11 15:38:05 +0000

    correct typo

--- src/components/molecules/Intro.tsx
+++ src/components/molecules/Intro.tsx
@@ -7,7 +7,7 @@ const Intro: React.FC = () => {
                 Chi è Serena?
             </Typography>
             <Typography variant="base" className="text-justify">
-                Megatesto su serena e la sua passione per lacucina che è una cosa che wow non ci si può
+                Megatesto su serena e la sua passione per la cucina che è una cosa che wow non ci si può
                 credere è proprio bravissima ma è proprio una passione fa cene squisite a casa sua una 
                 volta ci ha fatto il pollo fritto io le ho portato dei fiori
             </Typography>
