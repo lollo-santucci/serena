4f8cf7424c2ffba9cff7996687b1d3fc834f9a3d
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-11 15:19:44 +0000

    Intro molecule component

---
+++ src/components/molecules/Intro.tsx
@@ -0,0 +1,18 @@
+import Typography from "@/components/atoms/Typography";
+
+const Intro: React.FC = () => {
+    return (
+        <div className="flex flex-col">
+            <Typography variant="h3">
+                Chi è Serena?
+            </Typography>
+            <Typography variant="base" className="text-justify">
+                Megatesto su serena e la sua passione per lacucina che è una cosa che wow non ci si può
+                credere è proprio bravissima ma è proprio una passione fa cene squisite a casa sua una 
+                volta ci ha fatto il pollo fritto io le ho portato dei fiori
+            </Typography>
+        </div>
+    );
+};
+
+export default Intro;
