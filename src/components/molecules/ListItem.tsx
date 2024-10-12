9953f19478dba462144e9552fdc3c4aef275f52b
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-12 07:17:46 +0000

    add list item molecule component

---
+++ src/components/molecules/ListItem.tsx
@@ -0,0 +1,29 @@
+import Checkbox from "@/components/atoms/Checkbox";
+import Typography from "@/components/atoms/Typography";
+
+type ItemVersion = "ingredient" | "tool";
+
+interface ListItemProps {
+    version: ItemVersion;
+    name: string;
+    quantity?: string;
+    unit?: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ version, name, quantity, unit }) => {
+    const isTool = version === "tool";
+
+    return (
+        <div className="flex items-center justify-between py-1">
+            <div className="flex items-center">
+                <Checkbox />
+                <Typography variant="sm" className={`text-right ${isTool ? "hidden" : ""}`}>
+                    {unit} {quantity}
+                </Typography>
+                <Typography variant="sm" className="ml-2">{name}</Typography>
+            </div>
+        </div>
+    );
+};
+
+export default ListItem;
