79eca8f752316103249dec1867ce19553ed520fe
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-12 07:42:04 +0000

    add headline organism component

---
+++ src/components/organisms/Headline.tsx
@@ -0,0 +1,18 @@
+import Typography from "@/components/atoms/Typography";
+import Stats from "@/components/molecules/Stats";
+
+interface HeadlineProps {
+  title: string;
+  stats: { title: string; value: string }[];
+}
+
+const Headline: React.FC<HeadlineProps> = ({ title, stats }) => {
+  return (
+    <section className="flex flex-col md:flex-row justify-between items-center md:items-end">
+      <Typography variant="h1">{title}</Typography>
+      <Stats stats={stats}/>
+    </section>
+  );
+};
+
+export default Headline;
