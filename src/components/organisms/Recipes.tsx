d89792c70f8a5448ca7d3e41f490edf76731f311
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-14 13:43:04 +0000

    fix recipe component bug

--- src/components/organisms/Recipes.tsx
+++ src/components/organisms/Recipes.tsx
@@ -1,18 +1,31 @@
+import type { Recipe } from "@/types";
 import Typography from "@/components/atoms/Typography";
-import Recipe from "@/components/molecules/Recipe";
+import RecipeItem from "@/components/molecules/RecipeItem";
 
 interface RecipesProps {
     variant?: "list" | "recommended";
     title: string;
-    recipes: { imgPath: string; title: string; stats: { title: string; value: string }[] }[];
+    recipes: Recipe[];
 }
 
-const Recipes: React.FC<RecipesProps> = ({ variant="list", title, recipes }) => {
+const Recipes: React.FC<RecipesProps> = ({ variant = "list", title, recipes }) => {
     const isRecommended = variant === "recommended";
     return (
         <section className="flex flex-col items-center gap-6 w-full">
             <Typography variant="h1">{title}</Typography>
-            {recipes.map((recipe, index) => <Recipe key={index} variant={`${isRecommended ? "recommended" : "list"}`} imgPath={recipe.imgPath} title={recipe.title} stats={recipe.stats}/>)}
+            {recipes.map((recipe, index) =>
+                <RecipeItem
+                    key={index}
+                    variant={`${isRecommended ? "recommended" : "list"}`}
+                    imgPath={recipe.imagePath} 
+                    title={recipe.name}
+                    stats={[
+                        { title: "Preparazione", value: `${recipe.prepTime} min` },
+                        { title: "Cottura", value: `${recipe.cookTime} min` },
+                        { title: "Porzioni", value: `${recipe.serves}` },
+                    ]}
+                />
+            )}
         </section>
     );
 };
