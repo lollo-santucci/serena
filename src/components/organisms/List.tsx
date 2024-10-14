9fe76591805c0428d4b1f11e7369fa97ca6d05a9
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-14 16:02:17 +0000

    use types in List component

--- src/components/organisms/List.tsx
+++ src/components/organisms/List.tsx
@@ -1,25 +1,30 @@
+import type { Ingredient } from "@/types";
+import type { Tool } from "@/types";
 import ListItem from "@/components/molecules/ListItem";
 import Typography from "@/components/atoms/Typography";
 
 interface ListPros {
-    variant: "ingredient" | "tool";
-    items: { name: string; quantity: string; unit: string }[];
+    variant?: "ingredient" | "tool";
+    ingredients?: Ingredient[] 
+    tools?: Tool[];
 }
 
-const List: React.FC<ListPros> = ({ variant, items }) => {
+const List: React.FC<ListPros> = ({ variant = "ingredient", ingredients, tools }) => {
+    const items = variant === "ingredient" ? ingredients : tools;
+
     return (
         <section>
             <Typography variant="h3">
                 {variant === "ingredient" ? "Ingredienti" : "Strumenti"}
             </Typography>
             <div className="flex flex-col gap-2">
-                {items.map((item, index) => (
+                {items && items.map((item, index) => (
                     <ListItem
                         key={index}
                         version={variant}
-                        name={item.name}
-                        quantity={item.quantity}
-                        unit={item.unit}
+                        name={variant === "ingredient" && 'ingredient' in item ? `${item.ingredient}` : 'tool' in item ? `${item.tool}` : ''}
+                        quantity={variant === "ingredient" && 'ingredient' in item ? `${item.qt}` : ''}
+                        unit={variant === "ingredient" && 'ingredient' in item ? `${item.measure}` : ''}
                     />
                 ))}
             </div>
