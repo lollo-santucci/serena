dec4702d4c9785ee661aa8501b92fc787f66dd06
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-14 12:45:13 +0000

    test in pages

--- src/app/(public)/[taste]/page.tsx
+++ src/app/(public)/[taste]/page.tsx
@@ -1,5 +1,45 @@
-export default function Taste() {
-    return (
-      <h1>Taste under construction!</h1>
-    );
-  }
+import { GetStaticPropsContext } from "next";
+import type { Taste } from "@/types";
+import type { Recipe } from "@/types";
+import { fetchTastes } from "@/lib/api/tastes";
+import { fetchRecipes } from "@/lib/api/recipes";
+import Header from "@/components/organisms/Header";
+import Recipes from "@/components/organisms/Recipes";
+
+interface RecipePageProps {
+  taste: string;
+}
+
+export default async function Taste({ params }: { params: RecipePageProps }) {
+  const taste = params.taste;
+  const tastes: Taste[] = await fetchTastes();
+  // get taste id from tastes
+  const tasteId = tastes.find((t) => t.taste === taste)?.id;
+  // get recipes by taste id
+  const recipes: Recipe[] = await fetchRecipes('', tasteId);
+
+  return (
+    <>
+    <Header tastes={tastes} variant="sm" />
+      <main>
+        <Recipes title={taste} recipes={recipes.map((recipe) => ({
+          imgPath: '/imgs/copertina.webp',
+          title: recipe.name,
+          stats: [
+            { title: 'Preparazione', value: recipe.prepTime },
+            { title: 'Cottura', value: recipe.cookTime },
+            { title: 'Porzioni', value: recipe.serves },
+          ],
+        }))} />
+      </main>
+    </>
+  );
+}
+
+// Generazione SSG (Static Site Generation)
+export async function generateStaticParams() {
+  const tastes: Taste[] = await fetchTastes();
+  return tastes.map((taste) => ({
+    params: { taste: taste.taste },
+  }));
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,16 @@
-import Logo from "@/components/atoms/Logo";
-import Navigation from "@/components/molecules/Navigation";
+import { fetchTastes } from '@/lib/api/tastes';
+import type { Taste } from '@/types';
+import Logo from '@/components/atoms/Logo';
+import Navigation from '@/components/molecules/Navigation';
+import Search from '@/components/molecules/Search';
 
-export default function Home() {
+export default async function Home() {
+  const tastes: Taste[] = await fetchTastes();
+
   return (
     <>
-      <Logo version="bg-row" />
-      <Navigation/>
+      <Logo version="bg" />
+      <Navigation tastes={tastes} variant="row" />
     </>
   );
 }
