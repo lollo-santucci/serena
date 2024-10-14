d62cd54e92615621389df7ecbf5b481872b18955
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-14 13:42:28 +0000

    add recipe page

--- src/app/(public)/[taste]/page.tsx
+++ src/app/(public)/[taste]/page.tsx
@@ -13,24 +13,14 @@ interface RecipePageProps {
 export default async function Taste({ params }: { params: RecipePageProps }) {
   const taste = params.taste;
   const tastes: Taste[] = await fetchTastes();
-  // get taste id from tastes
   const tasteId = tastes.find((t) => t.taste === taste)?.id;
-  // get recipes by taste id
   const recipes: Recipe[] = await fetchRecipes('', tasteId);
 
   return (
     <>
     <Header tastes={tastes} variant="sm" />
       <main>
-        <Recipes title={taste} recipes={recipes.map((recipe) => ({
-          imgPath: '/imgs/copertina.webp',
-          title: recipe.name,
-          stats: [
-            { title: 'Preparazione', value: recipe.prepTime },
-            { title: 'Cottura', value: recipe.cookTime },
-            { title: 'Porzioni', value: recipe.serves },
-          ],
-        }))} />
+        <Recipes title={taste} recipes={recipes} />
       </main>
     </>
   );
