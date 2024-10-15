9465e658f28361627ac31786a18ef217972d3f87
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-15 13:16:59 +0000

    add unique export point for fetches

---
+++ src/lib/api/index.ts
@@ -0,0 +1,7 @@
+export { fetchTastes } from './tastes';
+export { fetchRecipes } from './recipes';
+export { fetchIngredients } from './ingredients';
+export { fetchSteps } from './steps';
+export { fetchImages } from './images';
+export { fetchTips } from './tips';
+export { fetchTools } from './tools';
