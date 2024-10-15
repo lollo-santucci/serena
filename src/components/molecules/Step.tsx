ee9faee1635b55d6fed922f5ad857ba66fd3191b
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-15 13:43:34 +0000

    use types in step componet

--- src/components/molecules/Step.tsx
+++ src/components/molecules/Step.tsx
@@ -1,18 +1,19 @@
+import type { Step as StepType } from "@/types";
+import type { Image as ImageType } from "@/types";
 import Typography from "@/components/atoms/Typography";
 import ImageGrid from "@/components/molecules/ImageGrid";
 
 interface StepProps {
-    index: number;
-    step: string;
-    specification?: string;
-    images?: { path: string }[];
+    n: number;
+    step: StepType;
+    images?: ImageType[];
 }
 
-const Step: React.FC<StepProps> = ({ index, step, specification, images }) => {
+const Step: React.FC<StepProps> = ({ n, step, images }) => {
     return (
         <div className="flex flex-col mx-6 gap-2">
-            <Typography variant="h5">{index}.&nbsp;{step}</Typography>
-            <Typography variant="base">&nbsp;&nbsp;&nbsp;&nbsp;{specification}</Typography>
+            <Typography variant="h5">{n}.&nbsp;{step.step}</Typography>
+            <Typography variant="base">&nbsp;&nbsp;&nbsp;&nbsp;{step.specification}</Typography>
             {images && <ImageGrid images={images} />}
         </div>
     );
