bcc34c748a63642681004d0938a31f54f4b4048b
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-16 07:00:35 +0000

    fix vercel deployment issues

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,14 +1,16 @@
-export default function debounce<T extends (...args: unknown[]) => void>(
+export default function debounce<T extends (...args: any[]) => any>(
   func: T,
   wait: number
-): (...args: Parameters<T>) => void {
+): (...args: Parameters<T>) => Promise<void> {
   let timeout: ReturnType<typeof setTimeout>; 
-  return function executedFunction(...args: Parameters<T>) {
-    const later = () => {
+  return function executedFunction(...args: Parameters<T>): Promise<void> {
+    return new Promise((resolve) => {
+      const later = () => {
+        clearTimeout(timeout);
+        resolve(func(...args));
+      };
       clearTimeout(timeout);
-      func(...args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+      timeout = setTimeout(later, wait);
+    });
   };
 }
