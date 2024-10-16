4859fe2a8074f0c2a20702c07aa3d917da5abf08
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-16 07:04:45 +0000

    specify string arg in debounce

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,13 +1,13 @@
-export default function debounce<T extends (...args: any[]) => any>(
+export default function debounce<T extends (arg: string) => Promise<void>>(
   func: T,
   wait: number
-): (...args: Parameters<T>) => Promise<void> {
+): (arg: string) => Promise<void> {
   let timeout: ReturnType<typeof setTimeout>; 
-  return function executedFunction(...args: Parameters<T>): Promise<void> {
+  return function executedFunction(arg: string): Promise<void> {
     return new Promise((resolve) => {
       const later = () => {
         clearTimeout(timeout);
-        resolve(func(...args));
+        resolve(func(arg));
       };
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
