863fd64749456d250040a7c539d47f684e2b9602
lollo-santucci <lorenz.santucci@gmail.com>
2024-10-16 07:08:24 +0000

    remove cosole.log

--- src/lib/api/recipes.ts
+++ src/lib/api/recipes.ts
@@ -27,8 +27,9 @@ export async function fetchRecipes(searchTerm?: string, tasteId?: number): Promi
         if (!response.ok) {
             throw new Error('Errore durante il recupero delle ricette');
         }
-        console.log('response', response);
+
         return await response.json();
+        
     } catch (error) {
         console.error('Errore durante il recupero delle ricette:', error);
         return [];
