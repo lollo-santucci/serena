b1fa07e0a84f7242b9f108771f21fdccee8ec1ba
lollo-santucci <lorenz.santucci@gmail.com>
2024-11-03 14:33:56 +0000

    fix header style

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -6,13 +6,14 @@ import Navigation from '@/components/molecules/Navigation';
 interface HeaderProps {
   variant?: 'sm' | 'bg-row' | 'bg-col';
   tastes: Taste[];
+  className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ tastes, variant = 'sm' }) => {
+const Header: React.FC<HeaderProps> = ({ tastes, variant = 'sm', className }) => {
   const isColumn = variant === 'bg-col';
 
   return (
-    <header className={`flex w-full ${variant.includes('bg') ? 'flex-col' : 'items-center justify-between'}`}>
+    <header className={`flex w-full justify-between ${variant.includes('bg') ? 'lg:flex-col' : 'items-center justify-between'} ${isColumn ? 'h-full' : ''} ${className}`}>
       <Logo version={variant.includes('bg') ? 'bg' : 'sm'} space={isColumn ? 'no-space' : 'space'}/>
       <Navigation tastes={tastes} variant={isColumn ? 'column' : 'row'} />
     </header>
