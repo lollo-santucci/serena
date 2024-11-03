217eff358e4b63f0537f46eec589293031eb532f
lollo-santucci <lorenz.santucci@gmail.com>
2024-11-03 14:36:02 +0000

    fix style

--- src/components/molecules/Intro.tsx
+++ src/components/molecules/Intro.tsx
@@ -2,11 +2,11 @@ import Typography from "@/components/atoms/Typography";
 
 const Intro: React.FC = () => {
     return (
-        <div className="flex flex-col">
-            <Typography variant="h3">
+        <div className="flex flex-col justify-between h-[100%]">
+            <Typography variant="h3" className="lg:!text-6xl">
                 Chi è Serena?
             </Typography>
-            <Typography variant="base" className="text-justify">
+            <Typography variant="base" className="text-justify lg:!text-4xl">
                 Megatesto su serena e la sua passione per la cucina che è una cosa che wow non ci si può
                 credere è proprio bravissima ma è proprio una passione fa cene squisite a casa sua una 
                 volta ci ha fatto il pollo fritto io le ho portato dei fiori
